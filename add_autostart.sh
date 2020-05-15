bd27c844bb97b34af3c2a0e1f9a362993b3e0ee1
Baloneo <alonebo.zhou@gmail.com>
2020-05-15 23:02:54 +0800

    update doc

--- add_autostart.sh
+++ add_autostart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo cp ./bin/network-ball-gtk /usr/bin/network-ball-gtk
+sudo cp ./releases/network-ball-gtk /usr/bin/network-ball-gtk
 mkdir -p ~/.config/autostart
 cp ./autostart/ball-network.desktop ~/.config/autostart/ball-network.desktop
 echo "success!"
